44e2813831cc637244217502b1bb5705e0a98c33
gitYoungren <youngrengitonga@gmail.com>
2022-11-27 00:17:49 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    let g = 2000
+    let m = 100
+    return g/ m
+}
+let x = 2
+const square = (x) => {
+return Math.pow(x, 2)
+} 
+let a = 3
+let b = 4
+const add = (a, b) =>{
+    return a + b
+}
